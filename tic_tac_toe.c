907922d0909926c9e311b42683510b9364d400f6
itsrealsumit007 <itsrealsumit007@gmail.com>
2024-07-07 21:19:34 +0530

    save

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -13,30 +13,42 @@ void initialize_board(char board[3][3]) {
 
 void display_board(char board[3][3]) {
     printf("\n");
-    printf(" %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
-    printf("---|---|---\n");
-    printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
-    printf("---|---|---\n");
-    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+    printf("  1   2   3 \n");
+    printf("1 %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+    printf(" ---|---|---\n");
+    printf("2 %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+    printf(" ---|---|---\n");
+    printf("3 %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
     printf("\n");
+}
+
+int is_board_full(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == ' ') {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int is_valid_move(char board[3][3], int row, int col) {
+    return (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ');
 }
 
 void player_move(char board[3][3], char player_marker) {
     int row, col;
     while (1) {
         printf("Player %c's turn. Enter row (1-3) and column (1-3): ", player_marker);
-        int result = scanf("%d %d", &row, &col);
-
-        if (result != 2) {
-            printf("Invalid input. Please enter numeric values.\n");
+        if (scanf("%d %d", &row, &col) != 2) {
+            printf("Invalid input. Please enter two numbers.\n");
             while (getchar() != '\n');
             continue;
         }
-
         row--;
         col--;
-
-        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+        if (!is_valid_move(board, row, col)) {
             printf("Invalid move. Please try again.\n");
         } else {
             board[row][col] = player_marker;
@@ -168,6 +180,46 @@ void reset_score(int *player_x_wins, int *player_o_wins) {
     printf("Scores have been reset.\n");
 }
 
+void save_game(char board[3][3], char current_player, int moves, int player_x_wins, int player_o_wins) {
+    FILE *file = fopen("tic_tac_toe_save.txt", "w");
+    if (file == NULL) {
+        printf("Failed to save game.\n");
+        return;
+    }
+    fprintf(file, "%c\n%d\n%d\n%d\n", current_player, moves, player_x_wins, player_o_wins);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            fprintf(file, "%c ", board[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+    printf("Game saved successfully.\n");
+}
+
+int load_game(char board[3][3], char *current_player, int *moves, int *player_x_wins, int *player_o_wins) {
+    FILE *file = fopen("tic_tac_toe_save.txt", "r");
+    if (file == NULL) {
+        printf("No saved game found.\n");
+        return 0;
+    }
+    fscanf(file, " %c\n%d\n%d\n%d\n", current_player, moves, player_x_wins, player_o_wins);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            fscanf(file, " %c", &board[i][j]);
+        }
+    }
+    fclose(file);
+    printf("Game loaded successfully.\n");
+    return 1;
+}
+
+void display_statistics(int player_x_wins, int player_o_wins) {
+    printf("Game Statistics:\n");
+    printf("Player X wins: %d\n", player_x_wins);
+    printf("Player O wins: %d\n", player_o_wins);
+}
+
 int main() {
     char play_again;
     int player_x_wins = 0;
@@ -175,22 +227,34 @@ int main() {
     int play_with_ai;
     int difficulty_level;
     char reset_scores;
+    char load_saved_game;
+    char save_game_choice;
+
+    printf("Do you want to load the saved game? (y/n): ");
+    scanf(" %c", &load_saved_game);
+    load_saved_game = tolower(load_saved_game);
+
+    char board[3][3];
+    char current_player = 'X';
+    int moves = 0;
+    if (load_saved_game == 'y') {
+        if (!load_game(board, &current_player, &moves, &player_x_wins, &player_o_wins)) {
+            initialize_board(board);
+        }
+    } else {
+        initialize_board(board);
+    }
 
     do {
         printf("Do you want to play against AI? (1 for Yes, 0 for No): ");
         scanf("%d", &play_with_ai);
-
         if (play_with_ai) {
             printf("Select difficulty level (1 for Easy, 2 for Hard): ");
             scanf("%d", &difficulty_level);
         }
 
-        char board[3][3];
-        initialize_board(board);
         display_board(board);
 
-        char current_player = 'X';
-        int moves = 0;
         int win = 0;
 
         while (moves < 9 && !win) {
@@ -217,13 +281,20 @@ int main() {
                 current_player = (current_player == 'X') ? 'O' : 'X';
                 moves++;
             }
+
+            printf("Do you want to save the game? (y/n): ");
+            scanf(" %c", &save_game_choice);
+            save_game_choice = tolower(save_game_choice);
+            if (save_game_choice == 'y') {
+                save_game(board, current_player, moves, player_x_wins, player_o_wins);
+            }
         }
 
         if (!win) {
             printf("It's a tie!\n");
         }
 
-        printf("\nScore:\nPlayer X: %d\nPlayer O: %d\n", player_x_wins, player_o_wins);
+        display_statistics(player_x_wins, player_o_wins);
 
         printf("Do you want to play again? (y/n): ");
         scanf(" %c", &play_again);
@@ -237,6 +308,10 @@ int main() {
             if (reset_scores == 'y') {
                 reset_score(&player_x_wins, &player_o_wins);
             }
+        } else {
+            initialize_board(board);
+            current_player = 'X';
+            moves = 0;
         }
 
     } while (play_again == 'y');
